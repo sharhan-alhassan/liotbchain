cc8fca0397838fcf42d4e25a1d41609dc103d26a
sharhan-alhassan <sharhanalhassan@gmail.com>
2024-06-07 03:12:02 +0000

    feat: fixing pytests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name='liotbchain',
-    version='0.0.2',
+    version='0.0.3',
     description='Python Lightweight IOT Blockchain framework',
     long_description=long_description,
     long_description_content_type='text/markdown',
